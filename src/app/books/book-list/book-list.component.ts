2a8069ee4974e6e6fe91b9354322b25162e41a32
Peter Dickten <peter.dickten@dcs-fuerth.de>
2024-05-15 09:40:11 +0200

    WIP

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -6,25 +6,31 @@ import { Component, TrackByFunction, ViewEncapsulation } from '@angular/core';
   styleUrl: './book-list.component.scss',
 })
 export class BookListComponent {
-  
-  books= [
-    {isbn: '12345678', title: 'The Great Gatsby', price: 7.99, coverUrl: 'https://m.media-amazon.com/images/I/71rUdxFkaCL._SL1500_.jpg'},
-    {isbn: '23456789', title: 'The Da Vinci Code', price: 9.99, coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SL1500_.jpg'},
+  books = [
+    {
+      isbn: '12345678',
+      title: 'The Great Gatsby',
+      price: 7.99,
+      coverUrl: 'https://m.media-amazon.com/images/I/71rUdxFkaCL._SL1500_.jpg',
+    },
+    {
+      isbn: '23456789',
+      title: 'The Da Vinci Code',
+      price: 9.99,
+      coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._SL1500_.jpg',
+    },
   ];
 
   coverIsVisible = true;
   searchTerm = '';
 
-  constructor() {
-  }
+  constructor() {}
 
   toggleCover() {
     this.coverIsVisible = !this.coverIsVisible;
   }
 
-  getBookId(book: any) {
+  getBookId(_index: number, book: any) {
     return book.isbn;
   }
-
-
 }
