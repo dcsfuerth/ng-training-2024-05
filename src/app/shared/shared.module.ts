b6daf405ae222fe5fb352495aa623e4c49bf739c
Peter Dickten <peter.dickten@dcs-fuerth.de>
2024-05-15 15:25:15 +0200

    WIP

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { RatingComponent } from './rating/rating.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CalculatorComponent, RatingComponent, WelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [],
 })
